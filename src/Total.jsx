026eb5d9a0142f68ffcc0180c2ee653bf96baca6
Franklin <fg1995ing@gmail.com>
2024-02-25 19:16:12 -0400

    push-2

---
+++ src/Total.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+const Total = (props) => {
+    return (
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    )
+  }
+
+export default Total;
